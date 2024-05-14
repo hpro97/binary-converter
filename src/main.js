1d288b2e116d62f62360860b9c439cfb81771552
Hpro97 <haxmo25@gmail.com>
2024-05-14 15:29:17 +0100

    hollowed out folder stucture

--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
-
 app.mount('#app')
+
+// js functionality for Binary
+
+// function addBinary(a,b) {
+//    let total = a + b;
+//      let binaryTotal = "";
+//    while (total > 0){
+//      let leftover = total % 2;
+//      binaryTotal = leftover + binaryTotal;
+//      total = Math.floor(total/2);
+//      }
+//    // const binaryTotal = total.toString(2) ;
+//    return binaryTotal;
+//    }
